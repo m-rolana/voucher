4b6520c8be076ba0c7dfa8369abc2f2ae0e369d7
Maria Mikhalska <rolana93@gmail.com>
2023-10-06 15:20:53 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import 'module-alias/register';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { getErrorMessage } from '@/services/error';
+import logger from '@/services/logger';
+
+const errorHandler: express.ErrorRequestHandler = (err, req, res, next) => {
+    if (!err) {
+        next();
+    }
+
+    // logger.error(err);
+
+    res.status(500).send(getErrorMessage(err));
+};
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(errorHandler);
+
+const server = app.listen(() => {
+    logger.info(`Server is listening at ${'localhost'}:${'3000'}`);
+});
+
+// graceful shutdown
+process.on('SIGINT', () => {
+    logger.info('Stopping server');
+
+    if (server) {
+        server.close(() => {
+            logger.info('Server stopped');
+            process.exit(1);
+        });
+    }
+});
---
+++ src/services/logger.ts
@@ -0,0 +1,38 @@
+import { createLogger, format, transports, addColors } from 'winston';
+
+const { combine, errors, timestamp, colorize, printf } = format;
+
+addColors({
+    error: 'red bold',
+    warn: 'yellow bold',
+    info: 'cyan',
+    debug: 'green bold',
+});
+
+const logger = createLogger({
+    level: 'debug',
+    format: combine(
+        errors({ stack: true }),
+        colorize({
+            all: true,
+        }),
+        timestamp(),
+        printf((info) => {
+            const { level, message, timestamp, stack } = info;
+            const stackLog = stack ? '\n' + stack : '';
+            return `[${timestamp}] ${level}: ${message} ${stackLog}`;
+        }),
+    ),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        // new transports.File({ filename: 'error.log', level: 'error' }),
+        // new transports.File({ filename: 'combined.log' }),
+        new transports.Console(),
+    ],
+});
+
+export default logger;
