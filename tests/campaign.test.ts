cace5a998b2b38086e72eb10b35b37c23cc0b9ac
Maria Mikhalska <rolana93@gmail.com>
2023-10-08 22:30:39 +0300

    minor fix

--- tests/campaign.test.ts
+++ tests/campaign.test.ts
@@ -5,7 +5,7 @@ describe('Campaign controller test', () => {
         it('Can create', async () => {
             const payload = { prefix: 'RECHARGE' };
 
-            const { data, status } = await apiRequest({ url: "campaigns", method: "POST", body: payload });
+            const { status } = await apiRequest({ url: "campaigns", method: "POST", body: payload });
 
             expect(status).toBe(200);
         })
